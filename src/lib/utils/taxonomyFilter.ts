78fc2e7886887fdb4f50716681966bc64361c045
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-09-29 00:09:37 +0300

    update

--- src/lib/utils/taxonomyFilter.ts
+++ src/lib/utils/taxonomyFilter.ts
@@ -1,8 +1,8 @@
 import { slugify } from "@/lib/utils/textConverter";
 
 const taxonomyFilter = (Apps: any[], name: string, key: any) =>
-  Apps.filter((apps) =>
-    apps.data[name].map((name: string) => slugify(name)).includes(key)
+  Apps.filter((app) =>
+    app.data[name].map((name: string) => slugify(name)).includes(key)
   );
 
 export default taxonomyFilter;
