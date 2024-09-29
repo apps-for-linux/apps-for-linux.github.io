f243a79875a119cf5828b6ca4b4bfdc6d6be45cc
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-09-30 00:21:09 +0300

    update

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -4,7 +4,7 @@ import dateFormat from "@/lib/utils/dateFormat";
 import { humanize, slugify } from "@/lib/utils/textConverter";
 import Fuse from "fuse.js";
 import { useEffect, useRef, useState } from "react";
-import { BiCalendarEdit, BiCategoryAlt } from "react-icons/bi";
+
 const { summary_length } = config.settings;
 
 export type SearchItem = {
