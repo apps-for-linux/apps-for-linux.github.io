d6214d0322c66b03317d8aa7ba573b2ad9db366f
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-10-20 13:19:18 +0300

    update shitty

--- shittyscraper.sh
+++ shittyscraper.sh
@@ -9,9 +9,9 @@
 
 cd $HOME/apps-for-linux.github.io/src/content/posts
 
-repo=$( cat 0ad.md | grep "Download" | cut -c 31- | sed -r 's/releases.+//' | sed '$ s/.$//' )
+repo=$( cat *.md | grep "Download" | cut -c 31- | sed -r 's/releases.+//' | sed '$ s/.$//' )
 
-curl -s https://api.github.com/repos/$repo/releases/latest \
-| grep "browser_download_url.*AppImage" \
-| cut -d : -f 2,3 \
-| tr -d \"
+current_version=$(curl -s https://api.github.com/repos/"$repo"/releases/latest | grep "browser_download_url.*AppImage" | cut -d : -f 2,3 | tr -d \")
+previous_version=$(grep "Download" | cut -c12-)
+
+update=$(sed -e s/$previous_version/$current_version/g "$filename".md )
