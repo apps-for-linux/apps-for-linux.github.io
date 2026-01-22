02d06818dfe8e4268fdc5cc38f3a40d651ce667f
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2026-01-22 11:44:27 +0000

    u

--- code/shittyscraper.sh
+++ code/shittyscraper.sh
@@ -7,11 +7,18 @@
 ### 5. export link
 ### 6. replace link
 
-cd $HOME/apps-for-linux.github.io/content/apps
+# Log all output to a file
+LOG=updater.log
+exec > >(tee -a "${LOG}") 2>&1
 
-repo=$( cat *.md | grep "Download" | cut -c 31-+ | sed -r 's/releases.+//' | sed '$ s/.$//' )
+
+dir=$HOME/apps-for-linux.github.io/content/apps
+
+repo=$( cat *.md | grep "Download" | cut -c 20 | sed -r 's/releases.+//' | sed '$ s/.$//' )
 
 current_version=$(curl -s https://api.github.com/repos/"$repo"/releases/latest | grep "browser_download_url.*AppImage" | cut -d : -f 2,3 | tr -d \")
 previous_version=$(grep "Download" | cut -c12- | tr -d '>')
 
-update=$(sed -e 's/$previous_version/$current_version/g' "$filename".md )
+for file in "$dir"/*.md; do
+    sed -i "s|$previous_version|$current_version|g" "$file"
+done
